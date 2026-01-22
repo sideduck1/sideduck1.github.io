ece9cb262141a29c7e44ac322225d273a3f59231
Flint <Eggebeenflint@gmail.com>
2026-01-22 09:24:39 +0100

    homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import { siteConfig } from "../siteConfig";
+import ProjectCard from "../components/ProjectCard";
+import projectData from "../data/projectdata.json";
 
 export default function Home() {
+    // Haal projects array uit de JSON
+  const projects = projectData.projects;
   return (
     <div>
       {/* Hero Section */}
       <section className="relative py-20 px-4">
+        {/* Projecten Section */}
+      <section className="py-16 px-4">
+        <div className="container mx-auto">
+          <h2 className="text-3xl font-bold text-(--text) mb-8 text-center">Mijn Projecten</h2>
+
+          {/* Grid met ProjectCards */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {projects.map((project) => (
+              <ProjectCard key={project.id} project={project} />
+            ))}
+          </div>
+        </div>
+      </section>
         
         {/* Decoratieve glow achter foto */}
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-64 h-64 bg-(--accent) rounded-full blur-3xl opacity-20"/>
